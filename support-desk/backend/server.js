ab07138be4a8b774bb986187381b1578bac217bd
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-17 17:51:37 +0100

    Basic Express Server Setup

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,1 +1,33 @@
-console.log("Server is running");
+const express = require("express"); // Express web server framework
+const dotenv = require("dotenv").config(); // Load environment variables from .env file
+
+const PORT = process.env.PORT || 5000; // Port to listen on
+const app = express(); // Create a new Express application
+
+// Create a route with express
+app.get("/", (req, res) => {
+  //   res.send("Hello World!"); // Send a response
+
+  // JSON Response
+  //   res.json({
+  //     message: "Welcome to the Support Desk API",
+  //   });
+
+  // JSON Response with 201 Create Status Code
+  //   res.status(201).json({
+  //     message: "Welcome to the Support Desk API",
+  //   });
+
+  // JSON Response with 20 OK Status Code
+  res.status(200).json({
+    message: "Welcome to the Support Desk API",
+  });
+});
+
+/* Send a GET request using Postman to test the server
+http://localhost:5000/
+*/
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+}); // Listen for requests on the given port
