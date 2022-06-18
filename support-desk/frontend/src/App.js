df5747e819c5dd7f776821404cfd15d5960256b3
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-18 19:42:22 +0100

    Header & Initial Pages init

--- support-desk/frontend/src/App.js
+++ support-desk/frontend/src/App.js
@@ -1,7 +1,28 @@
+// V6 React Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Import Components & Pages
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 import React from "react";
 
 function App() {
-  return <div>Frontend</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
