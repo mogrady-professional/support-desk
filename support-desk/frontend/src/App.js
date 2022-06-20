68561172068ced0e8af672b249af073b6811fe41
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-20 16:06:49 +0100

    Protected Route added for create new ticket route

--- support-desk/frontend/src/App.js
+++ support-desk/frontend/src/App.js
@@ -8,8 +8,10 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NewTicket from "./pages/NewTicket";
 
 import React from "react";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
---
+++ support-desk/frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewTicket() {
+  return <h1>New Ticket</h1>;
+}
+
+export default NewTicket;
