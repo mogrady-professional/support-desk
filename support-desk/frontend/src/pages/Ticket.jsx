4b29436a148377b5ef1f90e439f04c7ef84a7dc1
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-21 17:58:14 +0100

    Single Ticket Display UI

--- support-desk/frontend/src/pages/Ticket.jsx
+++ support-desk/frontend/src/pages/Ticket.jsx
@@ -23,9 +23,36 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId));
+    // eslint-disable-next-line
   }, [isError, message, ticketId]);
 
-  return <div>Ticket</div>;
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h3>Something Went Wrong</h3>;
+  }
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("en-UK")}
+        </h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Ticket;
